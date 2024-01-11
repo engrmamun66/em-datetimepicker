de7f813c105e2ae5ce1ff6c458506838239e357e
Al-mamun <engrmamun66@gmail.com>
2024-01-11 11:38:49 +0600

    Still working...

--- src/AppHolder.vue
+++ src/AppHolder.vue
@@ -1,11 +1,22 @@
 <script setup>
 import Picker from './pages/DatePicker.vue'
+let pickers = Array.from(document.querySelectorAll('.em-datetimepicker'));
+pickers.forEach(picker => {
+    let div = document.createElement('div');
+    div.style.position = 'relative';
+    div.insertAdjacentElement("beforebegin", picker);
+    div.appendChild(picker);
+    const parent = picker.parentElement;
+    if(parent) parent.appendChild(div);
+})
 </script>
 
 <template>  
-    <teleport to="body">
-        <Picker></Picker>
-    </teleport>
+    <template v-for="(picker, index) in pickers" :key="index">
+        <teleport :to="picker">
+            <Picker></Picker>
+        </teleport>
+    </template>
     
     
 </template>
