a16f761dda850d2083176b638fe351795e770b9d
Al-mamun <engrmamun66@gmail.com>
2024-01-17 10:53:51 +0600

    working...

--- src/pages/child/TimePicker.vue
+++ src/pages/child/TimePicker.vue
@@ -25,8 +25,6 @@ let selectedMinute = ref({
         "style": "left:50%;top:11%"
     })
 
-
-
 let hours_position = [
     {
         "id": 12,
@@ -111,355 +109,355 @@ let minutes_position = [
     {
         "id": 1,
         "value": "01",
-        "deg": "transform: rotate(60deg);",
+        "deg": "transform: rotate(6deg);",
         "style": "left:54.8%;top:4.3%"
     },
     {
         "id": 2,
         "value": "02",
-        "deg": "transform: rotate(120deg);",
+        "deg": "transform: rotate(12deg);",
         "style": "left:59.6%;top:5%"
     },
     {
         "id": 3,
         "value": "03",
-        "deg": "transform: rotate(180deg);",
+        "deg": "transform: rotate(18deg);",
         "style": "left:64.2%;top:6.3%"
     },
     {
         "id": 4,
         "value": "04",
-        "deg": "transform: rotate(240deg);",
+        "deg": "transform: rotate(24deg);",
         "style": "left:68.7%;top:8%"
     },
     {
         "id": 5,
         "value": "05",
-        "deg": "transform: rotate(300deg);",
+        "deg": "transform: rotate(30deg);",
         "style": "left:69.5%;top:16.2%"
     },
     {
         "id": 6,
         "value": "06",
-        "deg": "transform: rotate(360deg);",
+        "deg": "transform: rotate(36deg);",
         "style": "left:77%;top:12.8%"
     },
     {
         "id": 7,
         "value": "07",
-        "deg": "transform: rotate(420deg);",
+        "deg": "transform: rotate(42deg);",
         "style": "left:80.8%;top:15.8%"
     },
     {
         "id": 8,
         "value": "08",
-        "deg": "transform: rotate(480deg);",
+        "deg": "transform: rotate(48deg);",
         "style": "left:84.2%;top:19.2%"
     },
     {
         "id": 9,
         "value": "09",
-        "deg": "transform: rotate(540deg);",
+        "deg": "transform: rotate(54deg);",
         "style": "left:87.2%;top:23%"
     },
     {
         "id": 10,
         "value": "10",
-        "deg": "transform: rotate(600deg);",
+        "deg": "transform: rotate(60deg);",
         "style": "left:83.8%;top:30.5%"
     },
     {
         "id": 11,
         "value": "11",
-        "deg": "transform: rotate(660deg);",
+        "deg": "transform: rotate(66deg);",
         "style": "left:92%;top:31.3%"
     },
     {
         "id": 12,
         "value": "12",
-        "deg": "transform: rotate(720deg);",
+        "deg": "transform: rotate(72deg);",
         "style": "left:93.7%;top:35.8%"
     },
     {
         "id": 13,
         "value": "13",
-        "deg": "transform: rotate(780deg);",
+        "deg": "transform: rotate(78deg);",
         "style": "left:95%;top:40.4%"
     },
     {
         "id": 14,
         "value": "14",
-        "deg": "transform: rotate(840deg);",
+        "deg": "transform: rotate(84deg);",
         "style": "left:95.7%;top:45.2%"
     },
     {
         "id": 15,
         "value": "15",
-        "deg": "transform: rotate(900deg);",
+        "deg": "transform: rotate(90deg);",
         "style": "left:89%;top:50%"
     },
     {
         "id": 16,
         "value": "16",
-        "deg": "transform: rotate(960deg);",
+        "deg": "transform: rotate(96deg);",
         "style": "left:95.7%;top:54.8%"
     },
     {
         "id": 17,
         "value": "17",
-        "deg": "transform: rotate(1020deg);",
+        "deg": "transform: rotate(102deg);",
         "style": "left:95%;top:59.6%"
     },
     {
         "id": 18,
         "value": "18",
-        "deg": "transform: rotate(1080deg);",
+        "deg": "transform: rotate(108deg);",
         "style": "left:93.7%;top:64.2%"
     },
     {
         "id": 19,
         "value": "19",
-        "deg": "transform: rotate(1140deg);",
+        "deg": "transform: rotate(114deg);",
         "style": "left:92%;top:68.7%"
     },
     {
         "id": 20,
         "value": "20",
-        "deg": "transform: rotate(1200deg);",
+        "deg": "transform: rotate(120deg);",
         "style": "left:83.8%;top:69.5%"
     },
     {
         "id": 21,
         "value": "21",
-        "deg": "transform: rotate(1260deg);",
+        "deg": "transform: rotate(126deg);",
         "style": "left:87.2%;top:77%"
     },
     {
         "id": 22,
         "value": "22",
-        "deg": "transform: rotate(1320deg);",
+        "deg": "transform: rotate(132deg);",
         "style": "left:84.2%;top:80.8%"
     },
     {
         "id": 23,
         "value": "23",
-        "deg": "transform: rotate(1380deg);",
+        "deg": "transform: rotate(138deg);",
         "style": "left:80.8%;top:84.2%"
     },
     {
         "id": 24,
         "value": "24",
-        "deg": "transform: rotate(1440deg);",
+        "deg": "transform: rotate(144deg);",
         "style": "left:77%;top:87.2%"
     },
     {
         "id": 25,
         "value": "25",
-        "deg": "transform: rotate(1500deg);",
+        "deg": "transform: rotate(150deg);",
         "style": "left:69.5%;top:83.8%"
     },
     {
         "id": 26,
         "value": "26",
-        "deg": "transform: rotate(1560deg);",
+        "deg": "transform: rotate(156deg);",
         "style": "left:68.7%;top:92%"
     },
     {
         "id": 27,
         "value": "27",
-        "deg": "transform: rotate(1620deg);",
+        "deg": "transform: rotate(162deg);",
         "style": "left:64.2%;top:93.7%"
     },
     {
         "id": 28,
         "value": "28",
-        "deg": "transform: rotate(1680deg);",
+        "deg": "transform: rotate(168deg);",
         "style": "left:59.6%;top:95%"
     },
     {
         "id": 29,
         "value": "29",
-        "deg": "transform: rotate(1740deg);",
+        "deg": "transform: rotate(174deg);",
         "style": "left:54.8%;top:95.7%"
     },
     {
         "id": 30,
         "value": "30",
-        "deg": "transform: rotate(1800deg);",
+        "deg": "transform: rotate(180deg);",
         "style": "left:50%;top:89%"
     },
     {
         "id": 31,
         "value": "31",
-        "deg": "transform: rotate(1860deg);",
+        "deg": "transform: rotate(186deg);",
         "style": "left:45.2%;top:95.7%"
     },
     {
         "id": 32,
         "value": "32",
-        "deg": "transform: rotate(1920deg);",
+        "deg": "transform: rotate(192deg);",
         "style": "left:40.4%;top:95%"
     },
     {
         "id": 33,
         "value": "33",
-        "deg": "transform: rotate(1980deg);",
+        "deg": "transform: rotate(198deg);",
         "style": "left:35.8%;top:93.7%"
     },
     {
         "id": 34,
         "value": "34",
-        "deg": "transform: rotate(2040deg);",
+        "deg": "transform: rotate(204deg);",
         "style": "left:31.3%;top:92%"
     },
     {
         "id": 35,
         "value": "35",
-        "deg": "transform: rotate(2100deg);",
+        "deg": "transform: rotate(210deg);",
         "style": "left:30.5%;top:83.8%"
     },
     {
         "id": 36,
         "value": "36",
-        "deg": "transform: rotate(2160deg);",
+        "deg": "transform: rotate(216deg);",
         "style": "left:23%;top:87.2%"
     },
     {
         "id": 37,
         "value": "37",
-        "deg": "transform: rotate(2220deg);",
+        "deg": "transform: rotate(222deg);",
         "style": "left:19.2%;top:84.2%"
     },
     {
         "id": 38,
         "value": "38",
-        "deg": "transform: rotate(2280deg);",
+        "deg": "transform: rotate(228deg);",
         "style": "left:15.8%;top:80.8%"
     },
     {
         "id": 39,
         "value": "39",
-        "deg": "transform: rotate(2340deg);",
+        "deg": "transform: rotate(234deg);",
         "style": "left:12.8%;top:77%"
     },
     {
         "id": 40,
         "value": "40",
-        "deg": "transform: rotate(2400deg);",
+        "deg": "transform: rotate(240deg);",
         "style": "left:16.2%;top:69.5%"
     },
     {
         "id": 41,
         "value": "41",
-        "deg": "transform: rotate(2460deg);",
+        "deg": "transform: rotate(246deg);",
         "style": "left:8%;top:68.7%"
     },
     {
         "id": 42,
         "value": "42",
-        "deg": "transform: rotate(2520deg);",
+        "deg": "transform: rotate(252deg);",
         "style": "left:6.3%;top:64.2%"
     },
     {
         "id": 43,
         "value": "43",
-        "deg": "transform: rotate(2580deg);",
+        "deg": "transform: rotate(258deg);",
         "style": "left:5%;top:59.6%"
     },
     {
         "id": 44,
         "value": "44",
-        "deg": "transform: rotate(2640deg);",
+        "deg": "transform: rotate(264deg);",
         "style": "left:4.3%;top:54.8%"
     },
     {
         "id": 45,
         "value": "45",
-        "deg": "transform: rotate(2700deg);",
+        "deg": "transform: rotate(270deg);",
         "style": "left:11%;top:50%"
     },
     {
         "id": 46,
         "value": "46",
-        "deg": "transform: rotate(2760deg);",
+        "deg": "transform: rotate(276deg);",
         "style": "left:4.3%;top:45.2%"
     },
     {
         "id": 47,
         "value": "47",
-        "deg": "transform: rotate(2820deg);",
+        "deg": "transform: rotate(282deg);",
         "style": "left:5%;top:40.4%"
     },
     {
         "id": 48,
         "value": "48",
-        "deg": "transform: rotate(2880deg);",
+        "deg": "transform: rotate(288deg);",
         "style": "left:6.3%;top:35.8%"
     },
     {
         "id": 49,
         "value": "49",
-        "deg": "transform: rotate(2940deg);",
+        "deg": "transform: rotate(294deg);",
         "style": "left:8%;top:31.3%"
     },
     {
         "id": 50,
         "value": "50",
-        "deg": "transform: rotate(3000deg);",
+        "deg": "transform: rotate(300deg);",
         "style": "left:16.2%;top:30.5%"
     },
     {
         "id": 51,
         "value": "51",
-        "deg": "transform: rotate(3060deg);",
+        "deg": "transform: rotate(306deg);",
         "style": "left:12.8%;top:23%"
     },
     {
         "id": 52,
         "value": "52",
-        "deg": "transform: rotate(3120deg);",
+        "deg": "transform: rotate(312deg);",
         "style": "left:15.8%;top:19.2%"
     },
     {
         "id": 53,
         "value": "53",
-        "deg": "transform: rotate(3180deg);",
+        "deg": "transform: rotate(318deg);",
         "style": "left:19.2%;top:15.8%"
     },
     {
         "id": 54,
         "value": "54",
-        "deg": "transform: rotate(3240deg);",
+        "deg": "transform: rotate(324deg);",
         "style": "left:23%;top:12.8%"
     },
     {
         "id": 55,
         "value": "55",
-        "deg": "transform: rotate(3300deg);",
+        "deg": "transform: rotate(330deg);",
         "style": "left:30.5%;top:16.2%"
     },
     {
         "id": 56,
         "value": "56",
-        "deg": "transform: rotate(3360deg);",
+        "deg": "transform: rotate(336deg);",
         "style": "left:31.3%;top:8%"
     },
     {
         "id": 57,
         "value": "57",
-        "deg": "transform: rotate(3420deg);",
+        "deg": "transform: rotate(342deg);",
         "style": "left:35.8%;top:6.3%"
     },
     {
         "id": 58,
         "value": "58",
-        "deg": "transform: rotate(3480deg);",
+        "deg": "transform: rotate(348deg);",
         "style": "left:40.4%;top:5%"
     },
     {
         "id": 59,
         "value": "59",
-        "deg": "transform: rotate(3540deg);",
+        "deg": "transform: rotate(354deg);",
         "style": "left:45.2%;top:4.3%"
     }
 ]
