912bd48b4edd4ec4a8092e453d14eb14caa4f154
Al-mamun <engrmamun66@gmail.com>
2024-01-17 08:50:33 +0600

    working...

---
+++ src/pages/child/TimePicker.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, computed, reactive, defineProps, onMounted, inject, defineEmits } from 'vue';
+let { defaults, applyBtn } = defineProps({
+    defaults: { 
+        required: false,
+        default: {},
+    },
+    applyBtn: { 
+        required: false,
+        default: true,        
+    },
+});
+let emits = defineEmits(['onCancel', 'onApply', 'onToday']);
+function handleClick(from=''){
+    if(from == 'today'){
+        emits('onToday');
+    }
+    if(from == 'cancel'){
+        emits('onCancel');
+    }
+    if(from == 'apply'){
+        emits('onApply');
+    }
+}
+
+</script>
+
+<template>
+    <template v-if="defaults.buttons">
+        <template v-if="defaults.buttons?.todayBtn && (defaults.buttons?.cancelBtn || defaults.buttons?.applyBtn)">
+            <div class="flex-between">
+                <button class="btn-today" @click.stop="handleClick('today')">{{ defaults.buttons?.todayBtn }}</button>
+                 <div class="buttons">
+                    <button v-if="defaults.buttons?.cancelBtn" class="btn-cancel" @click.stop="handleClick('cancel')">{{ defaults.buttons?.cancelBtn }}</button>
+                    <button v-if="defaults.buttons?.applyBtn && applyBtn" class="btn-apply" @click.stop="handleClick('apply')">{{ defaults.buttons?.applyBtn }}</button>
+                </div>
+            </div>           
+        </template>
+        <template v-else>
+            <div class="buttons">
+                <button v-if="defaults.buttons?.cancelBtn" class="btn-cancel" @click.stop="handleClick('cancel')">{{ defaults.buttons?.cancelBtn }}</button>
+                <button v-if="defaults.buttons?.applyBtn && applyBtn" class="btn-apply" @click.stop="handleClick('apply')">{{ defaults.buttons?.applyBtn }}</button>
+            </div>
+        </template>
+    </template>
+</template>
+
+<style scoped>
+
+</style>
